dcaebaa3547c022cc0b3f19d9e53c3487a38a99c
Ashish Nevan <nevchris242@gmail.com>
2024-04-01 01:24:04 -0400

    Removed unneccessary files

--- src/main/java/edu/neu/coe/info6205/sort/classic/Classify.java
+++
@@ -1,5 +0,0 @@
-package edu.neu.coe.info6205.sort.classic;
-
-public interface Classify<X> {
-    int classify();
-}
--- src/test/java/edu/neu/coe/info6205/mcts/tictactoe/MCTSTest.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.coe.info6205.mcts.tictactoe;
-
-import static org.junit.Assert.*;
-
-public class MCTSTest {
-
-}
