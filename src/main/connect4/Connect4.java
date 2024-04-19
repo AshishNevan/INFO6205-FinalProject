4d543bd385f51eaa26f976917dc59db023976d00
Ashish Nevan <nevchris242@gmail.com>
2024-04-18 22:22:46 -0400

    Connect4 player names RED BLUE

--- src/main/connect4/Connect4.java
+++ src/main/connect4/Connect4.java
@@ -4,13 +4,28 @@ import core.Game;
 import core.State;
 
 import java.util.Optional;
+import java.util.Random;
 
-public class Connect4 implements Game {
-    public static final int red = 1;
-    public static final int green = 2;
+public class Connect4 implements Game<Connect4> {
+    public static final int RED = 1;
+    public static final int BLUE = 2;
     public static final int EMPTY = 0;
     public static final int ROWS = 6;
     public static final int COLUMNS = 7;
+
+    private final Random random;
+
+    public Connect4(Random random) {
+        this.random = random;
+    }
+
+    public Connect4() {
+        this(System.currentTimeMillis());
+    }
+
+    public Connect4(long seed) {
+        this(new Random(seed));
+    }
 
     public static void main(String[] args) {
         Connect4 game = new Connect4();
@@ -30,18 +45,18 @@ public class Connect4 implements Game {
 
         Optional<Integer> winner = state.winner();
         if (winner.isPresent()) {
-            System.out.println("Player " + (winner.get() == red ? "red" : "green") + " wins!");
+            System.out.println("Player " + (winner.get() == RED ? "red" : "green") + " wins!");
         } else {
             System.out.println("It's a draw!");
         }
     }
     @Override
-    public State start() {
+    public State<Connect4> start() {
         return new Connect4State();
     }
 
     @Override
     public int opener() {
-        return red;
+        return RED;
     }
 }
